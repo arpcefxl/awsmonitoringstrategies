ce2777b36ab7df55ea7a3e5f6b8010c0ca090a2e
Chad Smith <chad@brightkeycloud.com>
2019-01-21 11:39:14 -0800

    initial commit

---
+++ Cloudwatch_add_reachability_check.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+REGION=$1
+SNSTOPIC=$2
+
+#Generate a list of instance IDs for instances in RUNNING state
+INSTANCES=`/usr/local/bin/aws ec2 describe-instances --region $REGION --output text --filters "Name=instance-state-code,Values=16" --query Reservations[*].Instances[*].InstanceId |tr '\t' '\n'`
+
+#Generate a list of existing alarms to compare against
+#Executing this once prevents us from running this for every instance
+ALARMS=`/usr/local/bin/aws cloudwatch describe-alarms --alarm-name-prefix StatusCheckFailed-Alarm --state-value OK --output text --region $REGION  --query 'MetricAlarms[].Dimensions[].Value' | tr '\t' '\n'` 
+
+
+for i in `echo $INSTANCES`; do
+  INSTANCE=$i
+  echo "checking on $INSTANCE"
+
+  if [[ $ALARMS != *"$INSTANCE"* ]]; then
+    #Grab the value of the Name tag
+    name=`/usr/local/bin/aws ec2 describe-tags --region us-west-1 --filters "Name=resource-id,Values=$INSTANCE" "Name=key,Values=Name" --query Tags[*].Value`
+
+    #Create the new alarm
+    #Alarm name uses the name tag value
+    #SNS topic ARN provided on script command line
+    /usr/local/bin/aws cloudwatch put-metric-alarm --alarm-name StatusCheckFailed-Alarm_$name --alarm-description "Alarm when StatusCheckFailed metric has a value of one for two periods" --metric-name StatusCheckFailed --namespace AWS/EC2 --statistic Maximum --dimensions Name=InstanceId,Value=$INSTANCE --region $REGION --period 300 --unit Count --evaluation-periods 2 --threshold 1 --comparison-operator GreaterThanOrEqualToThreshold --alarm-actions $SNSTOPIC --insufficient-data-actions $SNSTOPIC
+    echo "creating alarms for $INSTANCE"
+  fi
+done
+
---
+++ db_freespace.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+REGION=$1
+DBID=vpc1w2-uberretailapi-prod
+MINSIZE=10000000000
+
+#linux date
+#tenminago=`date -u +%FT%TZ -d "10 minutes ago"`
+#now=`date -u +%FT%TZ`
+#macos date
+tenminago=`date -v -10M -u +%FT%TZ`
+now=`date -u +%FT%TZ`
+
+freesize=`aws --region $REGION cloudwatch get-metric-statistics \
+  --metric-name FreeStorageSpace --start-time $tenminago \
+  --end-time $now --period 3600 --namespace AWS/RDS \
+  --statistics Average \
+  --dimensions Name=DBInstanceIdentifier,Value=$DBID \
+  --query Datapoints[].Average`
+
+#Convert the output of the free size to an integer for easy comparison
+freesizeint=`echo ${freesize%.*}`
+
+echo "free size is $freesizeint"
+
+if [ "$freesizeint" -lt "$MINSIZE" ]; then
+  echo "DB volume will be extended due to limited space"
+
+#Figure out existing storage for DB instance
+  currentstorage=`aws rds describe-db-instances \
+  --db-instance-identifier $DBID --region $REGION \
+  --query DBInstances[].AllocatedStorage`
+
+  echo "The current storage size is $currentstorage"
+
+#Add 10Gb to the existing storage
+  NEWSTORAGE=$(($currentstorage + 10))
+  echo "Increasing storage size to $NEWSTORAGE"
+  #aws rds modify-db-instance --db-instance-identifier $DBID --allocated-storage $NEWSTORAGE --dry-run
+else
+  echo "DB size is fine"
+fi
